4c5be26930afa2ba6ba203eb0db827e91bc51c0f
geromedina <gerocode768@gmail.com>
2023-05-12 16:48:58 -0300

    Disney plus logo listo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css"
 import Login from './components/Login';
+import Header from './components/Header';
+import "./App.css"
 
 function App() {
   return (
   <div>
     <BrowserRouter>
+      <Header />
       <Routes>
           <Route path='/' element={<Login />} />
       </Routes>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,7 +9,7 @@ const Login = (props) => {
           <CTALogoOne src='/images/cta-logo-one.svg' alt=''/>
           <SignUp>GET ALL THERE</SignUp>
           <Description>Get Premier Access to Raya and The Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/23, the price of Disney+ and The Disney Bundle will increase by $1.</Description>
-          <CTALogoTwo src='images/cta-logo-two.png'/>
+          <CTALogoTwo src='images/cta-logo-two.png' alt=''/>
         </CTA>
         <BgImage />
       </Content>
